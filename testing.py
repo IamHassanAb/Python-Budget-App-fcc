3fd9558ff6ebc30c6d148bff4449cdc687536342
IamHassanAb <85189077+IamHassanAb@users.noreply.github.com>
2021-08-04 18:46:27 +0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+#This is only for testing out errors and problems related to the algorithm seperately.
 ledger = [{'amount': 1000, 'description': 'initial deposit'}, {'amount': -50, 'description': 'Transfer to Clothing'}, {'amount': 50, 'description': 'Transfer from Food'}, {'amount': -10.15, 'description': 'groceries'}, {'amount': -15.89, 'description': 'restaurant and more food for dessert'}]
 stri = "Food"
 cat_data = stri.center(30, '#') +"\n"
